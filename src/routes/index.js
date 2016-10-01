6d8e75ef863bc25f9aacafb9352ad9f2590252da
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-10-01 19:39:42 -0400

    Updated gitignore
    Ignore .env files

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+module.exports = (express) => {
+	const router = express.Router();
+
+	router.post('/status', (req, res) => {
+		res.json({
+			healthy: true,
+		})
+	});
+
+	//Routes
+	router.use('/api/', require('./api/user')(express));
+	
+	return router;
+}
--- src/server.js
+++ src/server.js
@@ -3,19 +3,30 @@ var body_parser = require('body-parser');
 var app = express();
 
 
-
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({
+	extended: true,
+}));
 
 // Config
 var port = 3000;
 
-app.use('/api/v1', require('../routes/api.js')(express));
-app.use(express.static('public'));
+
+app.use('/', require('./routes')(express));
+
+//app.use('/api/v1', require('../routes/api.js')(express));
+//app.use(express.static('public'));
 
 
 
+exports.server = app.listen(port, () => {
+	console.log('Server Active On', port);
+});
 
+/*
 var server = app.listen(port, function() {
 	console.log('Server Active on', port);
 });
+*/
 
-module.exports = server;
+//module.exports = server;
