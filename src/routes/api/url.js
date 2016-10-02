3799736ef0ff5390f16e812983b541d350567fcf
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-10-02 17:07:32 -0400

    Generate shortened url

--- src/routes/api/url.js
+++ src/routes/api/url.js
@@ -43,14 +43,47 @@ module.exports = (express) => {
 	});
 
 	// Create
+	
 	router.post('/url', (req, res) => {
+
+		// Generate a random string for shortened URL
+		// stringLength parameter is the length of the string output
+		function randomString(stringLength)
+		{
+		    var random_string = "";
+		    
+		    // Characters that are allowed to be used in random string generator
+		    var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+		    
+		    // Generate random string
+		    for( var i=0; i < stringLength; i++ )
+		        random_string += charset.charAt(Math.floor(Math.random() * charset.length));
+		    
+		    // Return the generated string
+		    return random_string;
+		}
+
+
+		// Shortened URL
+		var shortened_url = randomString(6);
+
+		req.body.shortened_url = shortened_url;
 
 		url.create(req.body, (err) => {
 			res.status(500).json(err);
 		}, (data) => {
+				
+			console.log(req.body.user_url);
+			console.log(req.body.shortened_url);
+			console.log(req.body);
+			
+
 			res.status(200).json(data);
 		})
+		
 	});
+	
+
 
 	return router;
 }
