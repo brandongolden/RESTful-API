86965e4d91de1477e251b579a8ce2ce7ba3a3cc2
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-10-01 21:40:04 -0400

    CRUD

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -3,11 +3,42 @@ const user = require('../../models/user');
 module.exports = (express) => {
 	const router = express.Router();
 
-
-
+	// Read All
 	router.get('/users', (req, res) => {
-		res.json({
-			healthy: true,
+		user.findAll( (err) => {
+			res.status(500).json(err);
+		}, (data) => {
+			res.status(200).json(data);
+		})
+	});
+
+	// Read One
+	router.get('/users/:id', (req, res) => {
+		req.body.id = req.params.id;
+		user.find(req.body, (err) => {
+			res.status(500).json(err);
+		}, (data) => {
+			res.status(200).json(data);
+		})
+	});
+
+	// Delete
+	router.delete('/users/:id', (req, res) => {
+		req.body.id = req.params.id;
+		user.destroy(req.body, (err) => {
+			res.status(500).json(err);
+		}, (data) => {
+			res.status(200).json(data);
+		})
+	});
+
+	// Update
+	router.post('/users/:id', (req, res) => {
+		req.body.id = req.params.id;
+		user.update(req.body, (err) => {
+			res.status(500).json(err);
+		}, (data) => {
+			res.status(200).json(data);
 		})
 	});
 
