63c92401e19a8bc361aee3a130e58093aa17a67a
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-10-06 11:09:58 -0400

    server.js

--- src/server.js
+++ src/server.js
@@ -1,31 +1,33 @@
-var express = require('express');
-var body_parser = require('body-parser');
-var app = express();
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
 
 
-app.use(body_parser.json());
-app.use(body_parser.urlencoded({
-	extended: true,
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
 }));
 
 // Config
-var port = 3000;
+const port = 3000;
 
 
 app.use('/', require('./routes')(express));
 
-//app.use('/api/v1', require('../routes/api.js')(express));
-//app.use(express.static('public'));
+// app.use('/api/v1', require('../routes/api.js')(express));
+// app.use(express.static('public'));
 
 
 exports.server = app.listen(port, () => {
-	console.log('Server Active On', port);
+  // console.log('Server Active On', port);
 });
 
 /*
 var server = app.listen(port, function() {
-	console.log('Server Active on', port);
+  console.log('Server Active on', port);
 });
 */
 
-//module.exports = server;
+// module.exports = server;
