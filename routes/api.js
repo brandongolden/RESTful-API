93eec300064de84b8d7e5c4cf85b6fd2a47ddc6f
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-10-06 11:38:20 -0400

    api.js

--- routes/api.js
+++ routes/api.js
@@ -1,37 +1,35 @@
 module.exports = function(express) {
-	var router = express.Router();
+  const router = express.Router();
 
-	router.get('/', function(req, res) {
-		res.json({hello: "world"});
-	});
+  router.get('/', function(req, res) {
+    res.json({ hello: 'world' });
+  });
 
-	router.get('/status', function(req, res) {
-		res.json({healthy: true});
-	});
+  router.get('/status', function(req, res) {
+    res.json({ healthy: true });
+  });
 
-	router.get('/user/:id', function(req, res) {
-		res.json({user: { id: req.params.id }});
-	});
+  router.get('/user/:id', function(req, res) {
+    res.json({ user: { id: req.params.id } });
+  });
 
 
-	router.get('/url/:url(*)', function(req, res) {
+  router.get('/url/:url(*)', function(req, res) {
+  // Generate a random string for shortened URL
+  // stringLength parameter is the length of the string output
+    function randomString(stringLength) {
+    const randomstring = '';
 
-		// Generate a random string for shortened URL
-		// stringLength parameter is the length of the string output
-		function randomString(stringLength)
-		{
-		    var random_string = "";
-		    
-		    // Characters that are allowed to be used in random string generator
-		    var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
-		    
-		    // Generate random string
-		    for( var i=0; i < stringLength; i++ )
-		        random_string += charset.charAt(Math.floor(Math.random() * charset.length));
-		    
-		    // Return the generated string
-		    return random_string;
-		}
+// Characters that are allowed to be used in random string generator
+      const charset = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+// Generate random string
+for( var i=0; i < stringLength; i++ ) {
+randomstring += charset.charAt(Math.floor(Math.random() * charset.length));
+
+// Return the generated string
+return randomstring;
+}
 
 
 		// Shortened URL
@@ -39,7 +37,7 @@ module.exports = function(express) {
 
 		// json output
 		res.json({url: { user_url: req.params.url, shortened_url: shortened_url }});
-	});
+  };
 
 	return router;
 }
