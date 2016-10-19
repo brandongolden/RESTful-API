54664bfa74eb7b18b99e773a57db189cb5fdc74c
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-10-19 12:32:16 -0400

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,43 @@
-var gulp = require('gulp');
+// Brandon Golden
+// Assignment 10: Automated Version Bumping
 
+
+// $ npm install --global gulp-cli
+// $ npm install --save-dev gulp
+// $ npm install --save-dev gulp-bump
+// $ npm install --save-dev gulp-util
+
+
+// cd ~/Desktop/URL-Shortener
+// Run gulp
+
+
+var gulp = require('gulp');
+var bump = require('gulp-bump');
+var gutil = require('gulp-util');
+
+
+
+// Patch - default
+// $ gulp
 gulp.task('default', function() {
-  // place code for your default task here
+    return gulp.src(['./bower.json', './package.json'])
+    .pipe(bump({type: "patch"}).on('error', gutil.log))
+    .pipe(gulp.dest('./'));
+});
+
+// Minor
+// $ gulp minor
+gulp.task('minor', function() {
+    return gulp.src(['./bower.json', './package.json'])
+    .pipe(bump({type: "minor"}).on('error', gutil.log))
+    .pipe(gulp.dest('./'));
+});
+
+// Major 
+// $ gulp major
+gulp.task('major', function() {
+    return gulp.src(['./bower.json', './package.json'])
+    .pipe(bump({type: "major"}).on('error', gutil.log))
+    .pipe(gulp.dest('./'));
 });
